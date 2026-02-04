d02bbbbb44eec842c0a39690202e59a8882ee950
MARCELO NEPOMUCENO <marcelo.nepomuceno@redeoba.com.br>
2026-02-04 20:45:57 -0300

    fix: remove skipDuplicates from seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,12 +4,11 @@ const prisma = new PrismaClient();
 
 async function main() {
   await prisma.loja.createMany({
-    skipDuplicates: true,
     data: [
       { digito: 1, nome: "Oba Centro" },
       { digito: 2, nome: "Oba Norte" },
       { digito: 3, nome: "Oba Sul" },
-      { digito: 4, nome: "Oba Campinas" },
+      { digito: 4, nome: "Oba Campinas" }
     ],
   });
 }
